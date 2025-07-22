d7c686d6407eb8e5c945b64785e2698723e5c18c
Dikshit-singh02 <dikshitsingh9973@gmail.com>
2025-07-22 16:48:07 +0530

    JavaScript Basics
    JavaScript Basics

---
+++ JS/app.js
@@ -0,0 +1,26 @@
+// let pencilPrice=10;
+// let eraserPrice=10;
+// console.log("Total Price is :",pencilPrice+eraserPrice,"Rupees");
+
+// let color="yellow";
+// switch(color){
+//     case "red":
+//         console.log("stop");
+//         break;
+//     case "yellow":
+//         console.log("Speed slow");
+//         break;
+//     case "Green":
+//         console.log("Go");
+//         break;
+//     default:
+//         console.log("Light not working")    
+// }
+
+alert("Something wrong")
+console.error("This is an error");
+console.warn("this is a warning");
+let name= prompt("Enter Your Name");
+let msg="Hello "+ name;
+// console.log(name);
+alert(msg);
