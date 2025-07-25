f62fb5a01e1a1c646c692d110bff29a2215af70a
Dikshit-singh02 <dikshitsingh9973@gmail.com>
2025-07-26 01:20:32 +0530

    learnings
    learnings

--- JS/app.js
+++ JS/app.js
@@ -17,10 +17,30 @@
 //         console.log("Light not working")    
 // }
 
-alert("Something wrong")
-console.error("This is an error");
-console.warn("this is a warning");
-let name= prompt("Enter Your Name");
-let msg="Hello "+ name;
-// console.log(name);
-alert(msg);
+// alert("Something wrong")
+// console.error("This is an error");
+// console.warn("this is a warning");
+// let name= prompt("Enter Your Name");
+// let msg="Hello "+ name;
+// // console.log(name);
+// alert(msg);
+
+
+let msg="     Hello  12 ";
+console.log(msg);
+console.log(msg.trim());
+console.log(msg.toLowerCase());
+console.log(msg.toUpperCase());
+let name="Dikshit";
+console.log(name.indexOf("ksh"));
+console.log(name.indexOf("mksh"));
+let mSG=msg.trim();
+// console.log(mSG);
+// console.log(mSG.toUpperCase());
+console.log(mSG.trim().toLocaleUpperCase());
+console.log(name.slice(2));
+console.log(name.replace("ksh","xxx"));
+console.log(name.repeat(2));
+let students=["dsr","aman","nandan"];
+console.log(students);
+
