b87ccf8564f6c31b9d797eb56118a37e94669874
Dikshit-singh02 <dikshitsingh9973@gmail.com>
2025-07-29 17:01:47 +0530

    loops
    loops learning

--- JS/app.js
+++ JS/app.js
@@ -26,25 +26,68 @@
 // alert(msg);
 
 
-let msg="     Hello  12 ";
-console.log(msg);
-console.log(msg.trim());
-console.log(msg.toLowerCase());
-console.log(msg.toUpperCase());
-let name="Dikshit";
-console.log(name.indexOf("ksh"));
-console.log(name.indexOf("mksh"));
-let mSG=msg.trim();
-// console.log(mSG);
-// console.log(mSG.toUpperCase());
-console.log(mSG.trim().toLocaleUpperCase());
-console.log(name.slice(2));
-console.log(name.replace("ksh","xxx"));
-console.log(name.repeat(2));
-let students=["dsr","aman","nandan"];
-console.log(students);
-console.log(students);
-let info=["shardha",23,23.32]
-console.log(info);
-info[2]="dsr";
-console.log(info);
+// let msg="     Hello  12 ";
+// console.log(msg);
+// console.log(msg.trim());
+// console.log(msg.toLowerCase());
+// console.log(msg.toUpperCase());
+// let name="Dikshit";
+// console.log(name.indexOf("ksh"));
+// console.log(name.indexOf("mksh"));
+// let mSG=msg.trim();
+// // console.log(mSG);
+// // console.log(mSG.toUpperCase());
+// console.log(mSG.trim().toLocaleUpperCase());
+// console.log(name.slice(2));
+// console.log(name.replace("ksh","xxx"));
+// console.log(name.repeat(2));
+// let students=["dsr","aman","nandan"];
+// console.log(students);
+// console.log(students);
+// let info=["shardha",23,23.32]
+// console.log(info);
+// info[2]="dsr";
+// console.log(info);
+// let nums=[[1,2],[3,4],[6,1],[12,1]];
+// console.log(nums);
+// console.log(nums[0][0]);
+
+
+// loops in js
+
+// for(let i=1;i<=15;i=i+2){
+//     // if(i%2==1){
+//     // console.log(i);
+//     // }
+//     console.log(i);
+// }
+// let n=prompt("Enter Number in for loop");
+// for(let i=1;i<=10;i++){
+//     console.log(`${n} X ${i}  = ${i*n}`);
+// }
+// let m=prompt("Enter Number for while loop");
+// let i=1;
+// while(i<=10){
+//      console.log(`${m} X ${i}  = ${i*m}`);
+//      i++;
+// }
+
+
+
+// let heroes=[
+//     ["ironman","spiderman","thor"],
+//     ["superman","wonder women","flash"]
+// ];
+// for(i=0;i<heroes.length;i++){
+//     console.log(i,heroes[i],heroes[i].length);
+//    for(j=0;j<heroes[i].length;j++){
+//     console.log(`j=${j}, ${heroes[i][j]}`)
+//    }
+// }
+
+
+
+let fruits=["mango","apple","banana","litchi","orange"];
+for(fruit of fruits){
+    console.log(fruit);
+}
---
+++ JS/practice.js
@@ -0,0 +1,23 @@
+// let game=[['X',null,'O'],[null,'X',null],['O',null,'X']];
+// console.log(game);
+// let str="ApNaCoLlEgE";
+// let idx=3;
+// if(str[idx]==str[idx].toLowerCase()){
+//     console.log("characterislowercase");}
+//     else{
+//         console.log("characterisnotlowercase");
+//     }
+
+
+// Guessing Game
+
+// let favMovie=prompt("Enter your favorate movie name");
+// let guess=prompt("Enter your guess movie name");
+// while(guess!=favMovie&&guess!='quit'){
+// console.log("Wrong");
+//     guess=prompt("wrong guess. Please try again");
+// }
+
+// if(guess==favMovie){
+//     console.log("congrats Your win");
+// }
