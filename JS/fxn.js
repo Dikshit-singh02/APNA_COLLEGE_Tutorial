8e1c8f5ad789f118ad664635698562d8a90f0e9f
Dikshit-singh02 <dikshitsingh9973@gmail.com>
2025-07-31 14:20:30 +0530

    function learning
    function learning

---
+++ JS/fxn.js
@@ -0,0 +1,20 @@
+// function printName(){
+//     let name=prompt("Tell me your Name:")
+//     console.log("hello ",name);
+// }
+// printName();
+// function sum(a,b){
+
+//     return a+b;
+// }
+//  let s=sum(4,5);
+//   console.log(s);
+
+
+
+  //function expression
+  const sum=function(a,b){
+    return a+b;
+  }
+
+  console.log(sum(5,122))
