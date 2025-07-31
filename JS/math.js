fed8f07bdbebf1b6c29e2e2a98ec7bd937db9156
Dikshit-singh02 <dikshitsingh9973@gmail.com>
2025-07-31 12:43:29 +0530

    objects literals and math function learning
    objects literals and math function learning

---
+++ JS/math.js
@@ -0,0 +1,8 @@
+//math object
+
+console.log(Math.abs(-12234));
+console.log(Math.pow(2,4));
+console.log(Math.floor(5.5));//nearest smallest integer value
+console.log(Math.ceil(5.5));//nearest largest integer value
+console.log(Math.random());
+console.log(Math.floor(Math.random()*100)+1);
