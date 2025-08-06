8f0c9ad07a210c91565579fa91e7011d16f5d2b6
Dikshit-singh02 <dikshitsingh9973@gmail.com>
2025-08-06 18:52:16 +0530

    some learnigs
    some learnigs

--- APNA_COLLEGE/DOM/app.js
+++ APNA_COLLEGE/DOM/app.js
@@ -2,4 +2,27 @@
 // for(let i=0;i<smallImage.length;i++){
 //     console.dir(smallImage[i].src);
 // }
+// console.dir(document.querySelector("h1"));
+// console.dir(document.querySelector("#description"));
+// console.dir(document.querySelector(".oldImg"));
+// console.dir(document.querySelectorAll(".div a"));
+// let para=document.querySelector("p");
 
+
+let p=document.createElement('p');
+
+p.innerText="I am Dsr";
+
+document.querySelector("body").append(p);
+p.style.color="red";
+
+let div=document.createElement('div');
+let h1=document.createElement('h1');
+let p1=document.createElement('p');
+
+h1.innerText="I am inside div "
+p1.innerText="Me Too";
+div.append(h1);
+div.append(p1);
+div.classList.add("box")
+document.querySelector("body").prepend(div)
