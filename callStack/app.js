2ffffb091208b23812e2e2c1c30ea7068375ceba
Dikshit-singh02 <dikshitsingh9973@gmail.com>
2025-08-13 17:48:21 +0530

    some changes
    some changes

---
+++ callStack/app.js
@@ -0,0 +1,16 @@
+// function hello(){
+//     console.log("inside hello fxn")
+//     console.log("hello");
+// }
+// function demo(){
+//     console.log("calling hello fxn")
+//     hello();
+// }
+
+// console.log("calling demo fxn")
+// demo();
+// console.log("done")
+
+
+let h1=document.querySelector('h1');
+h1.style.color="red";
