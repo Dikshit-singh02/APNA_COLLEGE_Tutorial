16bdc592b59218d3116fc4df74dba028dec5023d
Dikshit-singh02 <dikshitsingh9973@gmail.com>
2025-08-20 15:04:17 +0530

    some learnings
    some learnings

--- APNA_COLLEGE/Promises/app.js
+++ APNA_COLLEGE/Promises/app.js
@@ -32,25 +32,99 @@
 
 
 
-function savetoDb(data){
-    return new Promise((resolve,reject)=>{
- let internetSpeed=Math.floor(Math.random()*10)+1;
-    if(internetSpeed>4){
-       resolve("sucess :Data save"); 
-    }else{
-        reject("failure: Data not saved");
-    }
+// function savetoDb(data){
+//     return new Promise((resolve,reject)=>{
+//  let internetSpeed=Math.floor(Math.random()*10)+1;
+//     if(internetSpeed>4){
+//        resolve("sucess :Data save"); 
+//     }else{
+//         reject("failure: Data not saved");
+//     }
+//     });
+// }
+
+
+
+
+// savetoDb("Apna college")
+// .then(()=>{
+//     console.log("promised was ressolved");
+   
+// })
+// .catch(()=>{
+//     console.log("promise was rejected");
+// });
+
+
+// async function greet(){
+    // jkk   promish give rejected
+    // throw "weak connection"
+    //   return "hello World!";
+//     setTimeout(() => {
+//     console.log("Hello after 3 seconds");
+// }, 3000);
+// }
+// greet()
+// .then((result)=>{
+//     console.log("promish was ressolved");
+//      console.log("Result was:",result);
+// })
+// .catch((err)=>{
+//      console.log("promish was rejected due to error ",err);
+// })
+
+// let demo=async()=>{return 5};
+
+
+
+
+// function getNum(){
+//     return new Promise((ressolve,reject)=>{
+//         setTimeout(()=>{
+//              let num=Math.floor(Math.random()*10)+1;
+//              if(num>3){
+//                 reject("Promish rejected");
+//              }
+//         console.log(num);
+//         ressolve();
+//     },1000);
+//         })
+       
+// }
+// async function demo() {
+//     await  getNum();
+//     await getNum();
+
+    
+//     await getNum();
+//     await getNum();
+// }
+
+
+function getNum() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let num = Math.floor(Math.random() * 10) + 1;
+            console.log(num);
+            if (num > 3) {
+                reject("Promise rejected because number > 3");
+            } else {
+                resolve(num);
+            }
+        }, 1000);
     });
 }
 
+async function demo() {
+    try {
+        await getNum();
+        await getNum();
+        // await getNum();
+        // await getNum();
+        console.log("All numbers generated successfully!");
+    } catch (err) {
+        console.log("Error caught:", err);
+    }
+}
 
-
-
-savetoDb("Apna college")
-.then(()=>{
-    console.log("promised was ressolved");
-   
-})
-.catch(()=>{
-    console.log("promise was rejected");
-});
+demo();
