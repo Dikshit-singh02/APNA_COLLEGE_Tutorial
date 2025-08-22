8e60f48a1c8501002ad4769a59acb6aa7aac1c4d
Dikshit-singh02 <dikshitsingh9973@gmail.com>
2025-08-22 19:31:46 +0530

    Backend Learnings
    Backend Learnings

---
+++ APNA_COLLEGE/WebClassRoom/BACKEND/script.js
@@ -0,0 +1,16 @@
+// let n=5;
+// for(let i=0;i<n;i++){
+//     console.log("Hello "+i);
+// }
+// console.log("bye")
+// let args=process.argv;
+// for(let i=2;i<args.length;i++){
+// console.log("Hello to",args[i]);
+// }
+
+// const math=require("./math")
+// console.log(math)
+// console.log(math.sum(2,5));
+const info=require("./Fruits");
+console.log(info);
+console.log(info[0].name);
