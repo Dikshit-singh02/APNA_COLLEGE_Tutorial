a9efc0bf09fa832f2abb52e17a942c3628e09fe4
Dikshit-singh02 <dikshitsingh9973@gmail.com>
2025-12-01 16:21:09 +0530

    react components

---
+++ DELTAREACT/basic-react-app/src/Product.jsx
@@ -0,0 +1,11 @@
+import "./Product.css"
+function Product(){
+    return(
+        <div className="Product">
+            <h1>This is a product Tile</h1>
+            <h3>This is a Product Description.</h3>
+        </div>
+    )
+}
+
+export default Product;
---
+++ DELTAREACT/basic-react-app/src/Title.jsx
@@ -0,0 +1,9 @@
+function Title(){
+  return (
+    <>
+    <h1>I am the Title!</h1>
+    <p>2*2={2*2}</p>
+    </>
+  )
+}
+export default Title;
