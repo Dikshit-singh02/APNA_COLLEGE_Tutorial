51832c81b47c4dc0f843a40c135eb7b26c548267
Dikshit-singh02 <dikshitsingh9973@gmail.com>
2025-12-18 16:42:58 +0530

    learning States,Hooks and useState

---
+++ DELTAREACT/react-state-class/src/Counter.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+export default function Counter(){
+    //let [stateVariable,setStateVariable]=useState(10);
+
+    let [count,setCount]=useState(0);
+    //console.log(arr);
+
+    let incCount=()=>{
+        setCount(count+1);
+        
+    };
+    let decCount=()=>{
+        setCount(count-1);
+    }
+    return(
+        <div>
+            <h3>Count={count}</h3>
+            <button onClick={incCount}>Increase Count</button>
+            <button onClick={decCount}>Decrease Count</button>
+        </div>
+    )
+}
