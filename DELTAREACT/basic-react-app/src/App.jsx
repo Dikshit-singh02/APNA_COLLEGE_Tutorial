23abc823e00c3802d5d9ce8684b0204c816e2466
Dikshit-singh02 <dikshitsingh9973@gmail.com>
2026-01-24 17:32:34 +0530

    Lottery Game
    Lottery Game

--- DELTAREACT/basic-react-app/src/App.jsx
+++ DELTAREACT/basic-react-app/src/App.jsx
@@ -3,11 +3,13 @@ import Button from "./Button.jsx";
 import Hover from "./Hover.jsx"
 import ProductTab from "./ProductTab.jsx"
 import Form from "./Form.jsx";
+import Lottery from "./Lottery.jsx";
+import Ticket from "./Ticket.jsx";
+
 function App() {
   return(
     <>
-    {/* <Hover/> */}
-    <Form/>
+  <Lottery n={3} winningSum={15}/>
     </>
     
   ) ;
